e98dca38c2e4b570a63b1b6d374727f0c7af1966
Musire <Mcmusire@gmail.com>
2023-05-10 16:28:22 -0600

    updated z-index on sidebar display

--- client/src/components/navbar/Sidebar.jsx
+++ client/src/components/navbar/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
             <button className="md:hidden" onClick={toggleMenu}>
                 {!open && <img src={menu} alt="" className="z-50 w-6 h-5" />}
             </button>
-            {open && <aside className="z-50 fixed top-0 left-0 z-40 w-screen h-screen bg-black/75 centered " >
+            {open && <aside className=" fixed top-0 left-0 z-40 w-screen h-screen bg-black/75 centered " >
                 <section className="absolute top-0 left-0 flex flex-col w-3/5 h-full px-[2.75rem] py-8 bg-white gap-y-14">
                     <img src={close} alt="" className="z-50 w-6 h-6" onClick={toggleMenu} />
                     <ul className="flex flex-col gap-6">
